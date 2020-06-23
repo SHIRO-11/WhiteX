f523ea02cbede75f1bd5c981e64c7d7f67f3fb60
史朗出口 <howaito01@gmail.com>
2020-06-23 13:27:29 +0900

    最新コミット

--- index.php
+++ index.php
@@ -69,21 +69,54 @@
             <h1 class="new-archive-title">最新の投稿</h1>
             <?php while (have_posts()) : the_post(); //繰り返し処理開始?>
             <a href="<?php the_permalink(); //投稿（固定ページ）のリンクを取得?>">
+                <!-- 1つずつの記事 -->
                 <div class="article-content">
-                    <?php if (has_post_thumbnail()): ?>
-                    <img src="<?php the_post_thumbnail_url('full'); ?>"
-                        alt="各記事のサムネイル" />
-                    <?php else: ?>
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.jpg"
-                        alt="no-image" />
-                    <?php endif; ?>
-                    <h2 class="post-list-title"><?php the_title(); //投稿（固定ページ）のタイトルを表示?>
+                    <!-- 投稿日時を表示 パラメータで書式を指定 -->
+                    <div class="date"><?php the_time('Y.m.d'); ?>
+                    </div>
+                    <!-- 投稿（固定ページ）のタイトルを表示 -->
+                    <h2 class="post-list-title"><?php the_title();?>
                     </h2>
-                    <div class="date"><?php the_time('Y.m.d'); //投稿日時を表示 パラメータで書式を指定?>
+                    <!-- 投稿のカテゴリー -->
+                    <p class='post-category'><?php the_category(',');?>
+                    </p>
+                    <a
+                        href="<?php the_permalink(); //投稿（固定ページ）のリンクを取得?>">
+                        <?php if (has_post_thumbnail()): ?>
+                        <!-- サムネイルの表示 -->
+
+                        <img src="<?php the_post_thumbnail_url('full'); ?>"
+                            alt="各記事のサムネイル" />
+                        <?php else: ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.jpg"
+                            alt="no-image" />
+                        <?php endif; ?>
+                    </a>
+                    <!-- 記事の抜粋文 -->
+                    <div class="excerpt">
+                        <?php the_excerpt(); ?>
+                    </div>
+                    <!-- 続きを読むボタン -->
+                    <div class="read-more">
+                        <a
+                            href="<?php the_permalink(); //投稿（固定ページ）のリンクを取得?>">続きを読む</a>
                     </div>
                 </div>
             </a>
+
             <?php endwhile; // 繰り返し終了?>
+
+            <div class="posts-navigation">
+                <div class="nav-links">
+                    <div class="nav-previous">
+                        <?php previous_posts_link(); ?>
+                    </div>
+                    <div class="nav-next">
+                        <?php next_posts_link(); ?>
+                    </div>
+                </div>
+            </div>
+
         </div>
         <?php else : //条件分岐：投稿が無い場合は?>
 
@@ -92,6 +125,8 @@
                 href="<?php echo esc_url(home_url('/')); ?>">トップページに戻る</a>
         </p>
     </div>
+
+
     <?php endif; //条件分岐終了?>
 
     <?php get_sidebar(); //sidebar.phpを取得?>
